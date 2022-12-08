da9ade82cc802bd793ca1c7343637eb37201f16c
Artjoms Iskovs <mildbyte@gmail.com>
2022-12-08 21:36:20 +0000

    Optimize the table build query

--- src/s2sf/sql/daily_diff.sql
+++ src/s2sf/sql/daily_diff.sql
@@ -7,14 +7,11 @@ WITH snapshot_times AS (
     WHERE sg_image_created::timestamp > '2022-01-01 00:00:00'::timestamp
 ),
 all_observations AS (
-    SELECT * FROM (
-        SELECT
-            date,
-            LAG(date, 1) OVER (ORDER BY date) AS prev_date
-        FROM (SELECT DISTINCT snapshot_time_day AS date
-            FROM snapshot_times)
-    )
-    WHERE prev_date IS NOT NULL
+    SELECT
+        date,
+        LAG(date, 1) OVER (ORDER BY date) AS prev_date
+    FROM (SELECT DISTINCT snapshot_time_day AS date
+        FROM snapshot_times)
 ),
 daily_diff AS (
     SELECT
@@ -22,12 +19,14 @@ daily_diff AS (
         COALESCE(prev.id, curr.id) AS id,
         prev.id IS NULL AS is_added
     FROM
-        snapshot_times prev INNER JOIN all_observations ao ON ao.prev_date = prev.snapshot_time_day
+        snapshot_times prev
+        INNER JOIN all_observations ao
+            ON ao.prev_date = prev.snapshot_time_day
         FULL OUTER JOIN snapshot_times curr
-        ON ao.date = curr.snapshot_time_day
-        AND prev.id = curr.id
+            ON ao.date = curr.snapshot_time_day
+            AND prev.id = curr.id
     WHERE (prev.id IS NULL OR curr.id IS NULL)
-    -- Exclude the first scrape (prev_date is NULL -> looks like we added 40k datasets)
-    AND COALESCE(ao.date, curr.snapshot_time_day)::timestamp > '2022-08-08 00:00:00'::timestamp
+    -- Exclude the first scrape (no previous baseline to compare it to)
+    AND COALESCE(ao.date, curr.snapshot_time_day)::timestamp != (SELECT MIN(date) FROM all_observations)
 )
 SELECT date AS day, id, is_added FROM daily_diff ORDER BY 1, 2
